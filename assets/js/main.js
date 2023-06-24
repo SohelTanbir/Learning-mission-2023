a6c8e40f363eb4f7caac455c60bfa3e6691a44b9
SohelTanbir <soheltanbir19@gmail.com>
2023-06-24 11:24:59 +0600

    audio play next and previous and toggle play and pasue btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,66 +5,69 @@ const playBtn = document.getElementById("plya-btn");
 const pauseBtn = document.getElementById("pause-btn");
 const nextBtn = document.getElementById("next-btn");
 const innerCircleElement = document.querySelector(".inner-circle");
-const audioFile = "./assets/audio/audio.mp3";
+
 const audioFiles = [
+    "./assets/audio/audio-1.mp3",
     "./assets/audio/audio-2.mp3",
-    "./assets/audio/audio-6.mp3",
     "./assets/audio/audio-3.mp3",
     "./assets/audio/audio-4.mp3",
+    "./assets/audio/audio-5.mp3",
     "./assets/audio/audio-6.mp3",
-    "./assets/audio/audio-1.mp3",
-
 ];
 let currentIndext = 0;
-let previousIndex = 0;
 let nextIndex = 0;
 const audioObjects = [];
-
 for(let i = 0; i <audioFiles.length; i++) {
     const audio  = new Audio(audioFiles[i]);
     audioObjects.push(audio);
 }
 
-
-console.log(audioObjects);
-
-    
 function playAudio(index) {
-    const audio = audioObjects[index]
-    audio.play();
-    innerCircleElement.classList.add('playing');
-    console.log("playing =", index);
+        const audio = audioObjects[index];
+        audio.play();
+        innerCircleElement.classList.add('playing');
+
 }
     
 function pauseAudio(index) {
     const audio = audioObjects[index]
     audio.pause();
     innerCircleElement.classList.remove('playing');
-    console.log("pause =", index );
 }
 
 function previousAudio() {
     if(nextIndex >0){
         nextIndex--;
+        pauseAudio(nextIndex+1)
+        setAudioCurrentTime(nextIndex+1);
         playAudio(nextIndex);
-        console.log("prev index =", nextIndex);
     }
 }
 function nextAudio() {
-    if(nextIndex < audioObjects.length){
+    if(nextIndex < audioObjects.length-1){
         nextIndex++;
+        pauseAudio(nextIndex-1)
+        setAudioCurrentTime(nextIndex-1);
         playAudio(nextIndex);
-        console.log("nextIndex =", nextIndex);
     }
    
-      
+}
+
+// set audio current time
+function setAudioCurrentTime(index){
+    const audio = audioObjects[index];
+    audio.currentTime = 0
 }
 // add eventlistener 
 playBtn.addEventListener("click", function(e) {
-    playAudio(currentIndext)
+    playAudio(currentIndext);
+    pauseBtn.classList.remove("hide");
+    playBtn.classList.add("hide");
 })
 pauseBtn.addEventListener("click", function(e) {
-    pauseAudio()
+    pauseAudio(nextIndex);
+    pauseBtn.classList.add("hide");
+    playBtn.classList.remove("hide");
 })
 prevBtn.addEventListener("click", function(e) {
     previousAudio()
