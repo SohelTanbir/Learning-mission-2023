3cf46a05d70c1214108e7e205c87376c88434a07
SohelTanbir <soheltanbir19@gmail.com>
2023-08-13 23:16:13 +0600

    lean page routing

---
+++ src/app/contact/hello/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>Nested routing</p>
+        </div>
+    );
+};
+
+export default Hello;
